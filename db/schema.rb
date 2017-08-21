584ac03d6baa4203b4d0aa420e9b93f51023f063
RobinLeoKnauth <robinleoknauth@gmail.com>
2017-08-21 20:45:08 +0000

    password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821200749) do
+ActiveRecord::Schema.define(version: 20170821204413) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer "user_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170821200749) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
